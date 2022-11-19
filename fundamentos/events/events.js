b013795fd6284f1f3ace1d48d44d690634aea9f5
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-11-19 00:29:14 +0000

    Rest in Peace, Kevin Conroy!

--- fundamentos/events/events.js
+++ fundamentos/events/events.js
@@ -2,8 +2,17 @@ const { EventEmitter } = require('events')
 
 class Character extends EventEmitter {
     constructor(name) {
+        super()
         this.name = name
     }
 }
 
 const batman = new Character('Bruce Wayne')
+
+batman.on('batsignal', message => console.log(message))
+batman.on('knightfall', () => console.log(`My true name is ${batman.name}`))
+
+const RIP_KEVIN_CONROY = 'I AM THE DARKNESS, I AM THE NIGHT! I, AM BATMAN!'
+
+batman.emit('batsignal', RIP_KEVIN_CONROY)
+batman.emit('knightfall')
